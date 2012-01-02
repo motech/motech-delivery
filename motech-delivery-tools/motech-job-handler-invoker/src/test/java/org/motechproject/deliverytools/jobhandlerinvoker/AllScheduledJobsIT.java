bf8b60d8b9c0c6e50872c47b9d0b8d0ff4ec8f60
Sanchit Bahal <sanchit.bahal@gmail.com>
2012-01-02 17:53:55 +0530

    Revert "Luka, Sanchit | #920 | Upgraded Quartz scheduler from v1.8 to v2.1."
    This reverts commit 90c7e0d1f9e614fcc6ae6228b5ce10a25574ad48.

--- motech-delivery-tools/motech-job-handler-invoker/src/test/java/org/motechproject/deliverytools/jobhandlerinvoker/AllScheduledJobsIT.java
+++ motech-delivery-tools/motech-job-handler-invoker/src/test/java/org/motechproject/deliverytools/jobhandlerinvoker/AllScheduledJobsIT.java
@@ -9,7 +9,7 @@ import org.motechproject.util.DateUtil;
 import org.quartz.JobDataMap;
 import org.quartz.JobDetail;
 import org.quartz.SchedulerException;
-import org.quartz.Trigger;
+import org.quartz.SimpleTrigger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.test.context.ContextConfiguration;
@@ -17,10 +17,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
-import static org.quartz.JobBuilder.newJob;
-import static org.quartz.JobKey.jobKey;
-import static org.quartz.TriggerBuilder.newTrigger;
-import static org.quartz.TriggerKey.triggerKey;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"/applicationJobHandlerInvokerContextForTest.xml"})
@@ -34,24 +30,24 @@ public class AllScheduledJobsIT {
 
     @Before
     public void setUp() throws SchedulerException {
-        schedulerFactory.getScheduler().deleteJob(jobKey(jobName, MotechSchedulerServiceImpl.JOB_GROUP_NAME));
+        schedulerFactory.getScheduler().deleteJob(jobName, MotechSchedulerServiceImpl.JOB_GROUP_NAME);
     }
 
     @Test
     public void shouldLoadJobFromQuartzDataStore() throws SchedulerException {
-	    JobDataMap jobDataMap = new JobDataMap();
-	    jobDataMap.put("foo", "bar");
-	    JobDetail jobDetail = newJob(JobForAllScheduledJobsIT.class)
-		        .withIdentity(jobKey(jobName, MotechSchedulerServiceImpl.JOB_GROUP_NAME))
-			    .usingJobData(jobDataMap)
-		        .build();
+        JobDetail jobDetail = new JobDetail();
+        jobDetail.setName(jobName);
+        jobDetail.setGroup(MotechSchedulerServiceImpl.JOB_GROUP_NAME);
+        jobDetail.setJobClass(JobForAllScheduledJobsIT.class);
+        JobDataMap jobDataMap = new JobDataMap();
+        jobDataMap.put("foo", "bar");
+        jobDetail.setJobDataMap(jobDataMap);
 
-        Trigger simpleTrigger = newTrigger()
-		        .withIdentity(triggerKey("triggerName"))
-		        .startAt(DateUtil.now().plusDays(1).toDate())
-		        .build();
+        SimpleTrigger simpleTrigger = new SimpleTrigger();
+        simpleTrigger.setName("triggerName");
+        simpleTrigger.setStartTime(DateUtil.now().plusDays(1).toDate());
         schedulerFactory.getScheduler().scheduleJob(jobDetail, simpleTrigger);
-        ScheduledJob scheduledJob = allScheduledJobs.get(jobDetail.getKey().getName());
+        ScheduledJob scheduledJob = allScheduledJobs.get(jobDetail.getName());
         assertNotNull(scheduledJob);
         assertEquals("bar", scheduledJob.payload().get("foo"));
         assertNotNull(scheduledJob.triggerSummary(1).get(0));

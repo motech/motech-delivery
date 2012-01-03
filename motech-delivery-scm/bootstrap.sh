e1f20dae1d50ff7e0a61004a98579064b7b42855
shruthi <shruthidipali@gmail.com>
2012-01-03 18:13:59 +0530

    Dileep,Shruthi|#996|Adding bootstrap.sh
    To setup vm : wget https://github.com/motech/motech-delivery/raw/master/motech-delivery-scm/bootstrap.sh && ./bootstrap.sh

---
+++ motech-delivery-scm/bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# to setup vm : wget https://github.com/motech/motech-delivery/raw/master/motech-delivery-scm/bootstrap.sh && ./bootstrap.sh
+
+cd /tmp/ && wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-5.noarch.rpm && rpm -ivh /tmp/epel-release-6-5.noarch.rpm && \
+yum -y install puppet && \
+cd /tmp/ && wget --no-check-certificate http://github.com/motech/motech-delivery/tarball/master &&  \
+cd /tmp/ &&  tar xzvf /tmp/master &&  \
+cd /tmp/motech-motech-delivery*/motech-delivery-scm/src/main/resources/puppet  && \
+puppet apply manifests/site.pp --modulepath=modules/

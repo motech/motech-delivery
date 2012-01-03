11b670fb0a1430e00050ebf83f28b8553168b90a
Dileep Bapat <dileepbapat@gmail.com>
2012-01-03 18:26:52 +0530

    #996 | Dileep,Shruthi | Moving configuration params to separate file.

--- motech-delivery-scm/src/main/resources/puppet/manifests/nodes/nodes.pp
+++ motech-delivery-scm/src/main/resources/puppet/manifests/nodes/nodes.pp
@@ -1,11 +1,5 @@
 node default {
- $motechUser = "motech"
- $motechPassword = '$1$IW4OvlrH$Kui/55oif8W3VZIrnX6jL1'
- $mysqlPassword = "password"
- 
- $monitor_adminPhoneNumbers = "9880xxxxxx 9880xxxxxx"
- $monitor_kookooKey = "KK76eca1f1a1229c2cc44ee858843b426d"
- $monitor_environment = "production"
+ import "configuration"
  
  class{users : userName => "${motechUser}", password => "${motechPassword}" }	
 	

62e1644f160623eb905a1c369853b83ba05cbb0d
Dileep Bapat <dileepbapat@gmail.com>
2012-01-04 10:41:29 +0530

    #996 | Dileep, Preethi | Added documentation for generating password hash

--- motech-delivery-scm/puppet/manifests/nodes/configuration.pp
+++ motech-delivery-scm/puppet/manifests/nodes/configuration.pp
@@ -2,7 +2,7 @@
 #  Save and exit to continue with setup.
 #
  $motechUser = "motech"
- $motechPassword = '$1$IW4OvlrH$Kui/55oif8W3VZIrnX6jL1'
+ $motechPassword = '$1$IW4OvlrH$Kui/55oif8W3VZIrnX6jL1' #  echo "password" | openssl passwd -1 -stdin
  $mysqlPassword = "password"
  
  $monitor_adminPhoneNumbers = "9880xxxxxx 9880xxxxxx"

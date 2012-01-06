7632bac5bec8635561ab1a70f6902a13148f48b8
vijayaraj <vijayaraj@yahoo.com>
2012-01-06 10:09:54 +0530

    dileep | #000 | correction in script

--- motech-delivery-scm/bootstrap.sh
+++ motech-delivery-scm/bootstrap.sh
@@ -5,6 +5,5 @@
 cd /tmp/ && wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-5.noarch.rpm && rpm -ivh /tmp/epel-release-6-5.noarch.rpm && \
 yum -y install puppet && yum -y install git && \
 cd /tmp/ && git clone git://github.com/motech/motech-delivery.git && \
-cd /tmp/ &&  tar xzvf /tmp/master &&  \
-cd /tmp/motech-motech-delivery*/motech-delivery-scm/puppet  && vi manifests/nodes/configuration.pp && \
+cd /tmp/motech-motech-delivery/motech-delivery-scm/puppet  && vi manifests/nodes/configuration.pp && \
 puppet apply manifests/site.pp --modulepath=modules/

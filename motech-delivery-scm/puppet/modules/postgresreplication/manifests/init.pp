7fdb0a5acd0fec77b33a5dbeb7d764b2a55d9733
vijayaraj <vijayaraj@yahoo.com>
2012-01-19 14:17:58 +0530

    sushmita, vijay | #1107 | added owner for postgres repl

--- motech-delivery-scm/puppet/modules/postgresreplication/manifests/init.pp
+++ motech-delivery-scm/puppet/modules/postgresreplication/manifests/init.pp
@@ -1,27 +1,31 @@
 
 class postgresreplication ( $postgresMaster, $postgresSlave, $postgresUser) {
 
-    file {"create_master_postgresql_conf":
-        path => "/usr/local/pgsql/data/postgresql.conf",
+    file {"/usr/local/pgsql/data/postgresql.conf":
         content => template("postgresreplication/master_postgresql.erb"),
         owner => "${postgresUser}",
+        group => "${postgresUser}",
+  	    mode   =>  600,
     }
 
-    file {"create_master_pg_hba_conf":
-        path => "/usr/local/pgsql/data/pg_hba.conf",
+    file {"/usr/local/pgsql/data/pg_hba.conf":
         content => template("postgresreplication/master_pg_hba.erb"),
         owner => "${postgresUser}",
+        group => "${postgresUser}",
+  	    mode   =>  600,
     }
 
-    file {"create_slave_postgresql_conf":
-        path => "/usr/local/pgsql/data/postgresql.conf",
+    file {"/usr/local/pgsql/data/postgresql.conf":
         content => template("postgresreplication/slave_postgresql.erb"),
         owner => "${postgresUser}",
+        group => "${postgresUser}",
+  	    mode   =>  600,
     }
 
-    file {"create_slave_recovery_conf":
-        path => "/usr/local/pgsql/data/recovery.conf",
+    file {"/usr/local/pgsql/data/recovery.conf":
         content => template("postgresreplication/slave_recovery.erb"),
         owner => "${postgresUser}",
+        group => "${postgresUser}",
+  	    mode   =>  600,
     }
 }

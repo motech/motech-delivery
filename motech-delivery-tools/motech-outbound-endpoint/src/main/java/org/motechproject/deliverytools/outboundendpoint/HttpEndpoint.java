d8c7ebd63595c8279072c8daa9ba11a7019eba50
Arvind Kumar C <arvindc@thoughtworks.com>
2012-02-01 16:42:48 +0530

    Arvind | Included blank response for receive

--- motech-delivery-tools/motech-outbound-endpoint/src/main/java/org/motechproject/deliverytools/outboundendpoint/HttpEndpoint.java
+++ motech-delivery-tools/motech-outbound-endpoint/src/main/java/org/motechproject/deliverytools/outboundendpoint/HttpEndpoint.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 @RequestMapping("/motech-delivery-tools/outbound")
 @Controller
@@ -16,8 +18,9 @@ public class HttpEndpoint {
     private Logger logger = Logger.getLogger(this.getClass().getName());
 
     @RequestMapping(value = "receive", method = RequestMethod.GET)
-    public String receive(HttpServletRequest servletRequest) {
-        String queryString = servletRequest.getQueryString();
+    @ResponseBody
+    public String receive(HttpServletRequest servletRequest) throws UnsupportedEncodingException {
+        String queryString = URLDecoder.decode(servletRequest.getQueryString(), "UTf-8");
         logger.info(String.format("Received request: %s", queryString));
         lastRequest = new Request(queryString);
         return "Received";

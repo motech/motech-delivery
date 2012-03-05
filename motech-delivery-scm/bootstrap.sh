04ffbd9fb329f67be0ae5830ae10e0128121a8d8
Aravind SV <arvind.sv@gmail.com>
2012-03-05 16:56:47 +0530

    Duck | #000 | Improving the bootstrap script a bit.

--- motech-delivery-scm/bootstrap.sh
+++ motech-delivery-scm/bootstrap.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
-# to setup vm : wget https://github.com/motech/motech-delivery/raw/master/motech-delivery-scm/bootstrap.sh && sh ./bootstrap.sh
+# to setup vm : wget https://github.com/motech/motech-delivery/raw/master/motech-delivery-scm/bootstrap.sh && sh ./bootstrap.sh url_for_epel_rpm
 
-echo "# MoTeCH: Bootstrap Machine:"
-echo "# sh ./bootstrap.sh url_for_epel_rpm"
-echo "# EL5-32bit - http://download.fedoraproject.org/pub/epel/5/i386/epel-release-5-4.noarch.rpm"
-echo "# EL6-32bit - http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-5.noarch.rpm"
-echo "# EL6-64bit - http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-5.noarch.rpm"
+if [ "$#" -lt 1 ]; then
+  echo "MoTeCH: Bootstrap Machine:"
+  echo "Usage: sh ./bootstrap.sh url_for_epel_rpm\n"
+  echo "URLs:"
+  echo "EL5-32bit - http://download.fedoraproject.org/pub/epel/5/i386/epel-release-5-4.noarch.rpm"
+  echo "EL6-32bit - http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-5.noarch.rpm"
+  echo "EL6-64bit - http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-5.noarch.rpm"
+  exit 1
+fi
 
-cd /tmp/ && wget $1 --output-document=epel-release.rpm && rpm -ivh /tmp/epel-release.rpm && \
+cd /tmp/ && wget "$1" --output-document=epel-release.rpm && rpm -ivh /tmp/epel-release.rpm && \
 yum -y install puppet && yum -y install git && \
 cd /tmp/ && git clone git@github.com:motech/motech-delivery.git && \
-cd /tmp/motech-delivery/motech-delivery-scm/puppet  && vi manifests/nodes/configuration.pp && \
+cd /tmp/motech-delivery/motech-delivery-scm/puppet && vi manifests/nodes/configuration.pp && \
 puppet apply manifests/site.pp --modulepath=modules/

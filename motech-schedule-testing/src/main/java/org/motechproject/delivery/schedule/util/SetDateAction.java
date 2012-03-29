31facffe537b55f1773bfa02cb2e493bfa55429f
pchandra <pchandra@thoughtworks.com>
2012-03-29 17:12:24 +0530

    new schecule-testing library <praveen>

---
+++ motech-schedule-testing/src/main/java/org/motechproject/delivery/schedule/util/SetDateAction.java
@@ -0,0 +1,7 @@
+package org.motechproject.delivery.schedule.util;
+
+import org.joda.time.LocalDate;
+
+public abstract class SetDateAction {
+    public abstract void setTheDateTo(LocalDate dateToSetTo);
+}

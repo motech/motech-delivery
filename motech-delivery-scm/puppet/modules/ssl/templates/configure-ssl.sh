d6efbb387f7b6e8b458fd28e09bec9c9c38d68e3
pchandra <pchandra@thoughtworks.com>
2012-04-27 18:10:28 +0530

    praveen|#49|fixing typo for ssl configuration

--- motech-delivery-scm/puppet/modules/ssl/templates/configure-ssl.sh
+++ motech-delivery-scm/puppet/modules/ssl/templates/configure-ssl.sh
@@ -2,8 +2,8 @@ sed -i 's/\#LoadModule\ ssl_module\ modules\/mod_ssl.so/LoadModule\ ssl_module\ 
 
 sed -i 's/\#Include\ conf.d\/*.conf/Include\ conf.d\/*.conf/g' /etc/httpd/conf/httpd.conf
 
-sed -i 's/\#SSLCertificateFile/SLCertificateFile/g' /etc/httpd/conf.d/ssl.conf
+sed -i 's/\#SSLCertificateFile/SSLCertificateFile/g' /etc/httpd/conf.d/ssl.conf
 
-sed -i 's/\#SSLCertificateKeyFile/SLCertificateKeyFile/g' /etc/httpd/conf.d/ssl.conf
+sed -i 's/\#SSLCertificateKeyFile/SSLCertificateKeyFile/g' /etc/httpd/conf.d/ssl.conf
 
 

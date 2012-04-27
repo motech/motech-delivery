ae7def158bc68fa77184e2cb8613afb83eea9f6a
pchandra <pchandra@thoughtworks.com>
2012-04-27 18:06:02 +0530

    praveen |#79|adding shell script for apache ssl configuration

--- motech-delivery-scm/puppet/modules/ssl/templates/configure-ssl.sh
+++ motech-delivery-scm/puppet/modules/ssl/templates/configure-ssl.sh
@@ -2,7 +2,8 @@ sed -i 's/\#LoadModule\ ssl_module\ modules\/mod_ssl.so/LoadModule\ ssl_module\ 
 
 sed -i 's/\#Include\ conf.d\/*.conf/Include\ conf.d\/*.conf/g' /etc/httpd/conf/httpd.conf
 
-sed -i 's/\#SLCertificateFile/SLCertificateFile/g' /etc/httpd/conf.d/ssl.conf
+sed -i 's/\#SSLCertificateFile/SLCertificateFile/g' /etc/httpd/conf.d/ssl.conf
 
-sed -i 's/\#SLCertificateKeyFile/SLCertificateKeyFile/g' /etc/httpd/conf.d/ssl.conf
+sed -i 's/\#SSLCertificateKeyFile/SLCertificateKeyFile/g' /etc/httpd/conf.d/ssl.conf
+
 

32737a6c8ffffea76815c38ff0ac194d1a09b88b
Dileep Bapat <dileepbapat@gmail.com>
2012-05-14 12:39:30 +0530

    Deva,Dileep| #1443 | Building rpm url instead of expecting it in args.

--- motech-delivery-scm/bootstrap.sh
+++ motech-delivery-scm/bootstrap.sh
@@ -1,19 +1,12 @@
 #!/bin/sh
 
-# to setup vm : wget https://github.com/motech/motech-delivery/raw/master/motech-delivery-scm/bootstrap.sh && sh ./bootstrap.sh url_for_epel_rpm
+# to setup vm : wget https://raw.github.com/motech/motech-delivery/master/motech-delivery-scm/bootstrap.sh && sh ./bootstrap.sh 
 
-if [ "$#" -lt 1 ]; then
-  echo "MoTeCH: Bootstrap Machine:"
-  echo "Usage: sh ./bootstrap.sh url_for_epel_rpm\n"
-  echo "URLs:"
-  echo "EL5-32bit - http://download.fedoraproject.org/pub/epel/5/i386/epel-release-5-4.noarch.rpm"
-  echo "EL6-32bit - http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-5.noarch.rpm"
-  echo "EL6-64bit - http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-5.noarch.rpm"
-  exit 1
-fi
-
-cd /tmp/ && wget "$1" --output-document=epel-release.rpm && rpm -ivh /tmp/epel-release.rpm && \
+echo "MoTeCH: Bootstrap Machine:"
+rpmUrl="http://ftp.jaist.ac.jp/pub/Linux/Fedora/epel/6/"`uname -m`"/epel-release-6-6.noarch.rpm"
+echo "Using epel release 6 : $rpmUrl"
+cd /tmp && rpm -ivh "$rpmUrl"
 yum -y install puppet && yum -y install git && \
-cd /tmp/ && git clone git@github.com:motech/motech-delivery.git && \
+cd /tmp/ && git clone git://github.com/motech/motech-delivery.git && \
 cd /tmp/motech-delivery/motech-delivery-scm/puppet && vi manifests/nodes/configuration.pp && \
-puppet apply manifests/site.pp --modulepath=modules/
+puppet apply manifests/site.pp --modulepath=modules/ && echo "Completed!"

f49fbd78e9d25ed5cb57126a4487f1984f53cd5a
Balaji Narain <balajin@thoughtworks.com>
2012-10-30 00:27:09 +0530

    V2, BalajiN | #0 | Adding couchdb lucene optimization to batch
    1. Also upgrading spring version in delivery
    2. Changing the startergy for picking up default values for properties

--- motech-delivery-tools/motech-delivery-batch/motech-delivery-batch-lucene-optimization/src/main/java/org/motechproject/batch/jobs/luceneoptimization/component/LuceneOptimizer.java
+++ motech-delivery-tools/motech-delivery-batch/motech-delivery-batch-lucene-optimization/src/main/java/org/motechproject/batch/jobs/luceneoptimization/component/LuceneOptimizer.java
@@ -1,6 +1,5 @@
 package org.motechproject.batch.jobs.luceneoptimization.component;
 
-import org.motechproject.batch.jobs.luceneoptimization.component.CouchDbLuceneOptimizer;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -8,7 +7,6 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Properties;
@@ -16,10 +14,14 @@ import java.util.Properties;
 @Component
 public class LuceneOptimizer implements Tasklet, InitializingBean {
 
-    @Value("#{couchdb.lucene.host}")
     private String luceneHost;
-    @Value("#{couchdb.port}")
     private String couchdDbPort;
+
+    @Autowired
+    public LuceneOptimizer(@Qualifier("luceneOptimizationProperties") Properties couchLuceneProperties) {
+        luceneHost = couchLuceneProperties.getProperty("couchdb.lucene.host");
+        couchdDbPort = couchLuceneProperties.getProperty("couchdb.port");
+    }
 
     @Override
     public void afterPropertiesSet() throws Exception {

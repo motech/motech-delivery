5bfc14e26964554604b68c636687d4f45f53c3cd
Balaji Narain <balajin@thoughtworks.com>
2012-11-01 23:39:16 +0530

    BalajiN | #0 | Embedding deploy into batch so that dependencies can be managed better
    1. Also fixing bug with lucene index

--- motech-delivery-tools/motech-delivery-batch/motech-delivery-batch-common/src/main/java/org/motechproject/batch/utils/AntTask.java
+++ motech-delivery-tools/motech-delivery-batch/motech-delivery-batch-common/src/main/java/org/motechproject/batch/utils/AntTask.java
@@ -1,12 +1,23 @@
 package org.motechproject.batch.utils;
 
-import java.io.*;
+
+import org.apache.tools.ant.DefaultLogger;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.ProjectHelper;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 public class AntTask {
+
     private String buildFile;
     private String buildFilePath;
     private String environment;
     private String antFile;
+    private boolean embed = false;
 
     public AntTask(String buildFile, String buildFilePath, String environment, String antFile) {
         this.buildFile = buildFile;
@@ -15,8 +26,39 @@ public class AntTask {
         this.antFile = antFile;
     }
 
+    public AntTask(String buildFile, String buildFilePath, String environment, String antFile, boolean embed) {
+        this(buildFile, buildFilePath, environment, antFile);
+        this.embed = embed;
+    }
+
     public int run(String targetName) throws IOException, InterruptedException {
-        String cmd = "sudo -E "+ antFile +" -f " + buildFile + " " + targetName + " -Denv=" + environment + " -lib " + buildFilePath;
+        if (!embed)
+            return runExploded(targetName);
+        else
+            return runEmbedded(targetName);
+    }
+
+    private int runEmbedded(String targetName) {
+        Project project = new Project();
+        ProjectHelper helper = ProjectHelper.getProjectHelper();
+        project.addReference("ant.projectHelper", helper);
+        URL file = this.getClass().getClassLoader().getResource(buildFile);
+        if (null == file) {
+            throw new RuntimeException("Could not find the build file in path");
+        } else {
+            File source = new File(file.getFile());
+            DefaultLogger consoleLogger = initializeLogger();
+            helper.parse(project, source);
+            project.setBasedir(source.getParent());
+            project.addBuildListener(consoleLogger);
+            project.executeTarget(targetName);
+            project.setProperty("env", environment);
+        }
+        return 0;
+    }
+
+    private int runExploded(String targetName) throws IOException, InterruptedException {
+        String cmd = "sudo -E " + antFile + " -f " + buildFile + " " + targetName + " -Denv=" + environment + " -lib " + buildFilePath;
         Runtime runtime = Runtime.getRuntime();
         Process process = null;
         process = runtime.exec(cmd);
@@ -29,5 +71,13 @@ public class AntTask {
         }
 
         return process.exitValue();
+    }
+
+    private DefaultLogger initializeLogger() {
+        DefaultLogger consoleLogger = new DefaultLogger();
+        consoleLogger.setErrorPrintStream(System.err);
+        consoleLogger.setOutputPrintStream(System.out);
+        consoleLogger.setMessageOutputLevel(Project.MSG_INFO);
+        return consoleLogger;
     }
 }
--- motech-delivery-tools/motech-delivery-batch/motech-delivery-batch-services/src/main/java/org/motechproject/batch/jobs/services/StartAllServices.java
+++ motech-delivery-tools/motech-delivery-batch/motech-delivery-batch-services/src/main/java/org/motechproject/batch/jobs/services/StartAllServices.java
@@ -13,6 +13,8 @@ import org.springframework.stereotype.Component;
 import java.io.File;
 import java.util.Properties;
 
+import static org.apache.commons.lang.StringUtils.isNotBlank;
+
 @Component
 public class StartAllServices implements Tasklet, InitializingBean {
 
@@ -29,14 +31,18 @@ public class StartAllServices implements Tasklet, InitializingBean {
 
     @Override
     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-        String buildFilePath = batchProperties.getProperty("deploy.build.file");
-        String buildFile = buildFilePath + File.separator + "operate.xml";
+        String buildFilePath = prefix(batchProperties.getProperty("deploy.build.file"));
+        String buildFile = buildFilePath + "operate.xml";
         String environment = batchProperties.getProperty("environment");
         String antFile = batchProperties.getProperty("ant.file");
-        int returnCode = new AntTask(buildFile, buildFilePath, environment, antFile).run("start.services");
-        if(returnCode == 0)
+        int returnCode = new AntTask(buildFile, buildFilePath, environment, antFile, true).run("start.services");
+        if (returnCode == 0)
             return RepeatStatus.FINISHED;
         else
             throw new Exception("Return code from ant task is : " + returnCode + ". See the server logs for more details.");
+    }
+
+    private String prefix(String buildFilePath) {
+        return isNotBlank(buildFilePath) ? buildFilePath + File.separator : "";
     }
 }

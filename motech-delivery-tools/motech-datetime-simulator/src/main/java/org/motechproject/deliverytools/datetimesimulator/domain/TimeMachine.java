2aacfd42096e800c33d1de40855906503e9586b8
Aravind M <arav.psgtech@gmail.com>
2012-12-19 17:06:27 +0530

    Aravind | #00 | Branching out ananya seed.

--- motech-delivery-tools/motech-datetime-simulator/src/main/java/org/motechproject/deliverytools/datetimesimulator/domain/TimeMachine.java
+++
@@ -1,7 +0,0 @@
-package org.motechproject.deliverytools.datetimesimulator.domain;
-
-import org.motechproject.util.datetime.DateTimeSource;
-
-public interface TimeMachine extends DateTimeSource {
-    void update(String date, String hour, String minute);
-}
--- motech-delivery-tools/motech-document/src/main/java/org/motechproject/deliverytools/document/domain/Document.java
+++
@@ -1,9 +0,0 @@
-package org.motechproject.deliverytools.document.domain;
-
-import java.lang.annotation.*;
-
-@Target({ElementType.METHOD})
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface Document {
-}
